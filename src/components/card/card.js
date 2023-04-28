a8ba9978207aab38d8e2b361ea56bf804bb97ccb
chiragkalariya <chiragkalariya80@gmail.com>
2023-04-28 11:31:47 +0530

    changes in movie.js

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -4,7 +4,7 @@ import "./card.css"
 import React, { useEffect, useState } from 'react'
 import { Link } from "react-router-dom";
 
-const Card = ({movie}) => {
+const Card = ({ movie }) => {
     const [isLoadig, setLoading] = useState(true);
     // console.log(movie);
     useEffect(() => {
@@ -13,36 +13,36 @@ const Card = ({movie}) => {
         }, 1500)
     }, [])
     return <>
-            {
-                isLoadig 
-                ? 
+        {
+            isLoadig
+                ?
                 <div className="cards">
                     <SkeletonTheme baseColor="#444" highlightColor="#A9A9A9">
                         <Skeleton height={380} duration={2} />
                     </SkeletonTheme>
-                </div> 
-                : 
+                </div>
+                :
                 <Link to={`/movie/${movie.id}`}>
                     <div className="cards">
-                        <img src={`https://image.tmdb.org/t/p/original/${movie?movie.poster_path:""}`} className="cards_img" alt="" />
+                        <img src={`https://image.tmdb.org/t/p/original/${movie ? movie.poster_path : ""}`} className="cards_img" alt="" />
                         <div className="card_overlay">
-                            <h4 className="card_title">{movie?movie.original_title:""}</h4>
+                            <h4 className="card_title">{movie ? movie.original_title : ""}</h4>
                             <div className="card_runtime">
-                                {movie?movie.release_date:""}
+                                {movie ? movie.release_date : ""}
                                 <span className="card_rating">
-                                    {movie?movie.vote_average:""}
-                                    <span><i className="fa fa-star" aria-hidden="true"></i></span>   
-                                </span>    
+                                    {movie ? movie.vote_average : ""}
+                                    <span><i className="fa fa-star" aria-hidden="true"></i></span>
+                                </span>
                             </div>
                             <div className="card_description">
-                                {movie?movie.overview.slice(0, 118)+"...":""}
+                                {movie && movie.overview && movie.overview.slice(0, 118) + '...'}
                             </div>
                         </div>
                     </div>
                 </Link>
-            }
-        </>
-    
+        }
+    </>
+
 }
 
 export default Card
--- src/pages/movieDetail/movie.js
+++ src/pages/movieDetail/movie.js
@@ -7,67 +7,79 @@ const Movie = () => {
     const { id } = useParams()
     useEffect(() => {
         getData()
+        window.scrollTo(0, 0)
     }, [])
     const getData = () => {
         fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
             .then(res => res.json())
             .then(data => setMovie(data))
     }
-    console.log(currentMovieDetails);
+    // console.log(currentMovieDetails);
     return (
         <div className='movie_container'>
             <div className='container'>
-                <div className="movie_intro">
-                    <img className='movie_backdrop' src={`https://image.tmdb.org/t/p/original${currentMovieDetails ? currentMovieDetails.backdrop_path : ""}`} alt="ds" />
-                </div>
-                <div className="movie_detail">
-                    <div className="detail_left">
-                        <div className="movie_poster">
-                            <img src={`https://image.tmdb.org/t/p/original${currentMovieDetails ? currentMovieDetails.poster_path : ""}`} alt="" />
-                        </div>
+                <div className='movie_header'>
+                    <div className="movie_intro">
+                        <img className='movie_backdrop' src={`https://image.tmdb.org/t/p/original${currentMovieDetails ? currentMovieDetails.backdrop_path : ""}`} alt="ds" />
                     </div>
-                    <div className="detail_right">
-                        <div className="datail_rightop">
-                            <h2 className='movie_name'>{currentMovieDetails ? currentMovieDetails.original_title : ""}</h2>
-                            <h3 className="movie_tagline">{currentMovieDetails ? currentMovieDetails.tagline : ""}</h3>
-                            <div className="movie_rating">
-                                <span>{currentMovieDetails ? currentMovieDetails.vote_average : ""}</span>
-                                <span className='movie_votecount'>{currentMovieDetails ? currentMovieDetails.vote_count : ""}</span>
-                            </div>
-                            <h5 className='movie_runtime'>{currentMovieDetails ? currentMovieDetails.runtime : ""}</h5>
-                            <h5 className="movie_releasedate">{currentMovieDetails ? currentMovieDetails.release_date : ""}</h5>
-                            <div className="movie_generes">
-                                {
-                                    currentMovieDetails ?
-                                        currentMovieDetails.genres.map((gen) => (
-                                            <span className='movie_genre' id={gen.id}>{gen.name}</span>
-                                        )) :
-                                        ""
-                                }
+                    <div className="movie_detail">
+                        <div className="detail_left">
+                            <div className="movie_poster">
+                                <img src={`https://image.tmdb.org/t/p/original${currentMovieDetails ? currentMovieDetails.poster_path : ""}`} alt="" />
                             </div>
                         </div>
-                        <div className="detail_rightbottom">
-                            <h3 className='synopsistext'>Synopsis</h3>
-                            <p>{currentMovieDetails ? currentMovieDetails.overview : ""}</p>
+                        <div className="detail_right">
+                            <div className="datail_rightop">
+                                <h2 className='movie_name'>{currentMovieDetails ? currentMovieDetails.original_title : ""}</h2>
+                                <h3 className="movie_tagline">{currentMovieDetails ? currentMovieDetails.tagline : ""}</h3>
+                                <div className="movie_rating">
+                                    <span>{currentMovieDetails ? currentMovieDetails.vote_average : ""}</span>
+                                    <span className='movie_votecount'>{currentMovieDetails ? currentMovieDetails.vote_count : ""}</span>
+                                </div>
+                                <h5 className='movie_runtime'>{currentMovieDetails ? currentMovieDetails.runtime : ""}</h5>
+                                <h5 className="movie_releasedate">{currentMovieDetails ? currentMovieDetails.release_date : ""}</h5>
+                                <div className="movie_generes">
+                                    {
+                                        currentMovieDetails ?
+                                            currentMovieDetails.genres.map((gen) => (
+                                                <span className='movie_genre' id={gen.id}>{gen.name}</span>
+                                            )) :
+                                            ""
+                                    }
+                                </div>
+                            </div>
+                            <div className="detail_rightbottom">
+                                <h3 className='synopsistext'>Synopsis</h3>
+                                <p>{currentMovieDetails ? currentMovieDetails.overview : ""}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
                 <div className="movie_link">
                     <h4 className='movie_heading'>Useful Links</h4>
-                    <a className='movie__homeButton'>HomePbuttonge</a>
-                    <a className='movie__imdbButton'>Imdb</a>
+                    {
+                        currentMovieDetails && currentMovieDetails.homepage && <a href={currentMovieDetails.homepage} target='_blank' rel="noopener noreferrer" className='movie__homeButton'>Homepage<i className="fa fa-external-link" aria-hidden="true"></i></a>
+                    }
+                    {
+                        currentMovieDetails && currentMovieDetails.imdb_id && <a href={`https://www.imdb.com/title/${currentMovieDetails.imdb_id}`} target='_blank' rel="noopener noreferrer" className='movie__imdbButton'>Imdb<i className="fa fa-external-link" aria-hidden="true"></i></a>
+                    }
                 </div>
                 <h2 className='Production_heading'>Production companies</h2>
                 <div className="movie_production">
                     {
-                        currentMovieDetails ?
-                            currentMovieDetails.production_companies.map((comp) => (
-                                <div id={comp.id} className='movie_production_detail'>
-                                    <img src={`https://image.tmdb.org/t/p/original${comp.logo_path}`} alt="" />
-                                    <span className='movie_prod_compname'>{comp.name}</span>
-                                </div>
-                            )) :
-                            ""
+                        currentMovieDetails && currentMovieDetails.production_companies && currentMovieDetails.production_companies.map((comp) => (
+                            <>
+                                {
+                                    comp.logo_path
+                                    &&
+                                    <div className='movie_production_detail'>
+                                        <img src={`https://image.tmdb.org/t/p/original${comp.logo_path}`} alt="" />
+                                        <span className='movie_prod_compname'>{comp.name}</span>
+                                    </div>
+                                }
+                            </>
+                        )
+                        )
                     }
                 </div>
             </div>
